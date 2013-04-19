dcb32d0cb5ba0f6a89ac1bbb2b8829847b5c457c
MrKido <mrkidomain@inbox.lv>
2013-04-19 18:49:11 +0300

    BaseClass and ModInfo

---
+++ common/MrKido/SweeterVanilla/SweeterVanilla.java
@@ -0,0 +1,31 @@
+package MrKido.SweeterVanilla;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid="SweeterVanilla", version="pre0.0.1", name="Sweeter-Vanilla" )
+
+public class SweeterVanilla {
+
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent e){
+        
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent e){
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent e){
+        
+    }
+}
